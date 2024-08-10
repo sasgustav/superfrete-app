49d547d8f38af13351fad66c8a00a47994e0e807
Gustavo Vasconcelos <gustavo.vasconcelos@muvz.tech>
2024-08-09 21:59:20 -0300

    refactor: Improve UI layout and styling for message list

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,15 +1,19 @@
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSmile } from '@fortawesome/free-regular-svg-icons';
 
 const MessageList = ({ messages }) => {
   return (
-    <div className="message-list">
-      {messages.map((msg, index) => (
-        <div key={index} className="message-item">
-          <FontAwesomeIcon icon={faSmile} /> <p>{msg.text}</p>
-          <span>{new Date(msg.timestamp.seconds * 1000).toLocaleString()}</span>
-        </div>
-      ))}
+    <div className="message-section">
+      <h3>Mensagens enviadas</h3>
+      <div className="message-list">
+        {messages.map((msg, index) => (
+          <div key={index} className="message-item">
+            <p><FontAwesomeIcon icon={faSmile} /> {msg.text}</p>
+            <span>{new Date(msg.timestamp.seconds * 1000).toLocaleString()}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
